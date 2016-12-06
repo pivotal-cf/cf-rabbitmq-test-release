26a8070298f03b7a6faea7f7d47bdacc06e1403e
Scott Muc <smuc@pivotal.io>
2016-12-06 15:55:09 +0000

    Do not fail when rsyslogd config check exits non 0
    - the command `rsyslogd -d -N 2 2>&1` does not have consistent exit
      codes. The output will look the same regardless of the exit code
    
       e.g.: rsyslogd v8.22.0 will exit 1
             rsyslogd v8.23.0 will exit 0
    
    [#135348357]
    
    Signed-off-by: Carlo Colombo <ccolombo@pivotal.io>

--- jobs/syslog-configuration-test/templates/post-deploy.sh
+++ jobs/syslog-configuration-test/templates/post-deploy.sh
@@ -5,9 +5,9 @@
 # shellcheck disable=SC1091
 . /var/vcap/jobs/syslog-configuration-test/bin/watched-log-files.sh
 
-RSYSLOG_CONFIG_CHECK="$(rsyslogd -d -N 2 2>&1)"
 
 main() {
+  load_ryslogd_config_check
   ensure_rsyslog_imfile_module_loaded
   ensure_rsyslog_watches_log_files
 }
@@ -15,6 +15,20 @@ main() {
 fail() {
   echo "$*"
   exit 1
+}
+
+
+# because of differing behaviours of rsyslogd versions, we cannot assume that
+# this command will exit 0 with identical configuration.
+#
+# e.g.: rsyslogd v8.22.0 will exit 1
+#       rsyslogd v8.23.0 will exit 0
+#
+# The output will look the same regardless of the exit code
+load_ryslogd_config_check() {
+  set +e
+  RSYSLOG_CONFIG_CHECK="$(rsyslogd -d -N 2 2>&1)"
+  set -e
 }
 
 ensure_rsyslog_imfile_module_loaded() {
