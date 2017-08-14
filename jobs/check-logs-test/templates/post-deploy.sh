86047d83e4b3ad0cd1bfbf58b15ede962f21f3d5
Alberto Leal <aleal@pivotal.io>
2017-08-14 18:38:12 +0100

    Simplify log tests
    [#150173565]

--- jobs/check-logs-test/templates/post-deploy.sh
+++ jobs/check-logs-test/templates/post-deploy.sh
@@ -10,19 +10,6 @@
 main() {
   local file_to_inspect
 
-  local start_timestamp
-  start_timestamp=$(date +%s)
-  sleep 1
-  local port=5672
-
-  echo 0091AMQP | nc localhost $port
-
-  RETVAL=$?
-  if [[ $RETVAL != 0 ]]; then
-    echo "Failed to send a message to port $port"
-    exit 1
-  fi
-
   echo "About to inspect files..."
   for file_to_inspect in "${FILES_TO_INSPECT[@]}"
   do
@@ -34,13 +21,6 @@ main() {
     if [[ -z "$file" ]]
     then
       echo "Unable to find file in [${dir_name}] matching pattern [${file_expression}]"
-      exit 1
-    fi
-
-    mod_ts="$(ls -lc --time-style=+%s "$file" | awk '{ print $6 }')"
-    if [[ ! $mod_ts -gt $start_timestamp ]]
-    then
-      echo "Expected file [${file_to_inspect}] to have been modified"
       exit 1
     fi
   done
