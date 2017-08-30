ff77351411c7e694803e472e380aae4ffb607696
Vlad Stoian <vstoian@pivotal.io>
2017-08-30 16:49:43 +0100

    Add test for file descriptor limits
    [#149734315]
    
    Signed-off-by: Alberto Leal <aleal@pivotal.io>

---
+++ jobs/check-file-descriptor-limits-test/templates/post-deploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+[ -z "$DEBUG" ] || set -x
+
+export PATH="$PATH:/var/vcap/packages/rabbitmq-server/bin/:/var/vcap/packages/erlang/bin:/var/vcap/packages/jq-1.5/bin"
+
+main() {
+  # shellcheck disable=1091
+  . "/var/vcap/packages/bash-test-helpers/common.bash"
+
+  # shellcheck disable=SC1091
+  . "/var/vcap/jobs/check-file-descriptor-limits-test/bin/rabbitmq-credentials.sh"
+
+  check_file_descriptor_limit_has_been_set_correctly
+}
+
+
+check_file_descriptor_limit_has_been_set_correctly() {
+  current_node_name="$(rabbitmqctl eval "node().")"
+  current_fd_total="$(curl -u "$RABBITMQ_MANAGEMENT_USERNAME":"$RABBITMQ_MANAGEMENT_PASSWORD" http://localhost:15672/api/nodes/$current_node_name | jq -r .fd_total)"
+
+	log "Current node ($current_node_name) has fd_total: $current_fd_total"
+
+  if [[ "$current_fd_total" -ne "${RABBITMQ_FILE_DESCRIPTOR_LIMIT}" ]]
+  then
+    fail "Expected fd_limit to be $RABBITMQ_FILE_DESCRIPTOR_LIMIT but it was $current_fd_total"
+  fi
+}
+
+main
--- src/bash-test-helpers/common.bash
+++ src/bash-test-helpers/common.bash
@@ -3,8 +3,18 @@
 set -eu
 
 fail() {
-  echo "[FAIL] in $0" >> "/var/vcap/sys/log/cf-rabbitmq-release-test-failures.log"
-  echo "$*" >> "/var/vcap/sys/log/cf-rabbitmq-release-test-failures.log"
+  local message="${1?:missing message}"
+  local caller_name="${FUNCNAME[1]}"
+
+  echo "[FAIL][$caller_name] $message" >> "/var/vcap/sys/log/cf-rabbitmq-release-test-failures.log"
+
   exit 1
+}
+
+log() {
+  local message="${1?:missing message}"
+  local caller_name="${FUNCNAME[1]}"
+
+  echo "[DEBUG][$caller_name] $message" >> "/var/vcap/sys/log/cf-rabbitmq-release-test-debug.log"
 }
 
