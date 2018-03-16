6c0882ab26718619e2ffc8cd18ccca8ad0c7b813
Michal Kuratczyk <mkuratczyk@pivotal.io>
2018-03-16 16:36:47 +0100

    Add rabbitmq server version to the bin path
    [#155509097]
    
    Signed-off-by: George Blue <gblue@pivotal.io>

--- jobs/check-file-descriptor-limits-test/templates/post-deploy.sh
+++ jobs/check-file-descriptor-limits-test/templates/post-deploy.sh
@@ -4,7 +4,8 @@ set -e
 
 [ -z "$DEBUG" ] || set -x
 
-export PATH="$PATH:/var/vcap/packages/rabbitmq-server/bin/:/var/vcap/packages/erlang/bin:/var/vcap/packages/jq-1.5/bin"
+. /var/vcap/jobs/rabbitmq-server/etc/rabbitmq-server-version
+export PATH="$PATH:/var/vcap/packages/rabbitmq-server-$RMQ_SERVER_VERSION/bin/:/var/vcap/packages/erlang/bin:/var/vcap/packages/jq-1.5/bin"
 
 main() {
   # shellcheck disable=1091
